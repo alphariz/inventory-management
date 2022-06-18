3e080c84239a2dcaa250ed07d116cf9c09050540
Alfa <rizymuhammad@gmail.com>
2022-06-18 22:07:17 +0700

    Add location folder, and departement, rooms .blade.php

---
+++ resources/views/location/departement.blade.php
@@ -0,0 +1,5 @@
+<x-app-layout title="Departemen">
+    @slot('main')
+    Departemen
+    @endslot
+</x-app-layout>
---
+++ resources/views/location/rooms.blade.php
@@ -0,0 +1,5 @@
+<x-app-layout title="Ruangan">
+    @slot('main')
+    Ruangan
+    @endslot
+</x-app-layout>
